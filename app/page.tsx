22246a2bd62278e9d4a8e0e571d275976172f639
Mariya B <supermariyabros@gmail.com>
2024-07-14 21:04:09 +0300

    ✨ Update SEO

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,32 @@ import Testimonials from '@/components/HomePage/Testimonials';
 import Pricing from '@/components/HomePage/Pricing';
 import Contact from '../components/HomePage/Contact';
 
+import Head from 'next/head';
+import { siteConfig } from '@/config/site';
+
 export default function Home() {
   return (
     <section>
+      <Head>
+        <title>{siteConfig.name} - Learn English Online</title>
+        <meta
+          name="description"
+          content="Tutee - Learn English with personalized tutoring sessions"
+        />
+        <meta
+          name="keywords"
+          content="Tutee, Tutee English, online English tutor, English lessons"
+        />
+        <meta name="author" content="Tutee" />
+        <meta property="og:title" content="Tutee - Your Online English Tutor" />
+        <meta
+          property="og:description"
+          content="Tutee - Learn English with personalized tutoring sessions"
+        />
+        {/* <meta property="og:image" content="/path/to/your/image.jpg" /> */}
+        <meta property="og:url" content="https://tutee.co.uk" />
+      </Head>
+
       <Hero />
       <div className="new-section"></div>
       <About />
