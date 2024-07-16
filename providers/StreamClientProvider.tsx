1c5d8026fdf59d8e42eba9d3aee0854f22f665be
Mariya B <supermariyabros@gmail.com>
2024-07-16 18:51:31 +0300

    ✨ Remove chat

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { ReactNode, useEffect, useState, createContext, useContext } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
-import { StreamChat } from 'stream-chat';
 import { useUser } from '@clerk/nextjs';
 
 import { tokenProvider } from '@/actions/stream.actions';
@@ -10,66 +9,30 @@ import Loader from '@/components/Loader';
 
 const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-interface StreamContextType {
-  videoClient: StreamVideoClient | null;
-  chatClient: StreamChat | null;
-}
-
-const StreamContext = createContext<StreamContextType | undefined>(undefined);
-
-const StreamProvider = ({ children }: { children: ReactNode }) => {
-  const [videoClient, setVideoClient] = useState<StreamVideoClient | null>(null);
-  const [chatClient, setChatClient] = useState<StreamChat | null>(null);
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
   const { user, isLoaded } = useUser();
 
   useEffect(() => {
-    if (!isLoaded || !user || !API_KEY) return;
+    if (!isLoaded || !user) return;
+    if (!API_KEY) throw new Error('Stream API key is missing');
 
-    const chatClient = StreamChat.getInstance(API_KEY);
-    chatClient.connectUser(
-      {
-        id: user.id,
-        name: user.username || user.id,
-        image: user.imageUrl,
-      },
-      tokenProvider
-    );
-    setChatClient(chatClient);
-
-    const videoClient = new StreamVideoClient({
+    const client = new StreamVideoClient({
       apiKey: API_KEY,
       user: {
-        id: user.id,
-        name: user.username || user.id,
-        image: user.imageUrl,
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
       },
       tokenProvider,
     });
 
-    setVideoClient(videoClient);
-
-    return () => {
-      chatClient.disconnectUser();
-      setVideoClient(null);
-      setChatClient(null);
-    };
+    setVideoClient(client);
   }, [user, isLoaded]);
 
-  if (!videoClient || !chatClient) return <Loader />;
+  if (!videoClient) return <Loader />;
 
-  return (
-    <StreamContext.Provider value={{ videoClient, chatClient }}>
-      <StreamVideo client={videoClient}>{children}</StreamVideo>
-    </StreamContext.Provider>
-  );
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
 };
 
-export const useStream = () => {
-  const context = useContext(StreamContext);
-  if (!context) {
-    throw new Error('useStream must be used within a StreamProvider');
-  }
-  return context;
-};
-
-export default StreamProvider;
+export default StreamVideoProvider;
