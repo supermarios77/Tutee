bc59dd3c11378668dfba36e4483eff5c72a5e232
Mariya B <supermariyabros@gmail.com>
2024-07-19 18:02:50 +0300

    ✨ Add Links To Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import {
   Navbar as NextUINavbar,
@@ -8,26 +8,34 @@ import {
   NavbarBrand,
   NavbarItem,
   NavbarMenuItem,
-} from "@nextui-org/navbar";
-import { Link } from "@nextui-org/link";
-import { Button } from "@nextui-org/button";
+} from '@nextui-org/navbar';
+import { Link } from '@nextui-org/link';
+import { Button } from '@nextui-org/button';
+import { useReducer } from 'react';
+import { link as linkStyles } from '@nextui-org/theme';
+import { siteConfig } from '@/config/site';
+import NextLink from 'next/link';
+import clsx from 'clsx';
+import { ThemeSwitch } from '@/components/theme-switch';
+import { LanguageSwitcher } from './lang-switcher';
+import { SignedIn, SignedOut, useSession } from '@clerk/nextjs';
 
-import { useReducer } from "react";
-import { link as linkStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import NextLink from "next/link";
-import clsx from "clsx";
-
-import { ThemeSwitch } from "@/components/theme-switch";
-import { LanguageSwitcher } from "./lang-switcher";
-
+const checkUserRole = (session: any) => {
+  return session?.sessionClaims?.metadata?.role === 'admin';
+};
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useReducer((current) => !current, false);
+  const { session } = useSession();
+  const isAdmin = checkUserRole(session);
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+    <NextUINavbar
+      maxWidth="xl"
+      position="sticky"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
@@ -39,8 +47,8 @@ export const Navbar = () => {
             <NavbarItem key={item.href}>
               <NextLink
                 className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium'
                 )}
                 color="foreground"
                 href={item.href}
@@ -59,12 +67,40 @@ export const Navbar = () => {
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
           <LanguageSwitcher />
+          <SignedOut>
+            <NavbarItem>
+              <NextLink href="/sign-up">
+                <Button>Sign Up</Button>
+              </NextLink>
+            </NavbarItem>
+          </SignedOut>
+          <SignedIn>
+            <NavbarItem>
+              <NextLink href={isAdmin ? '/dashboard' : '/student-dashboard'}>
+                <Button>Dashboard</Button>
+              </NextLink>
+            </NavbarItem>
+          </SignedIn>
         </NavbarItem>
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <ThemeSwitch />
         <LanguageSwitcher />
+        <SignedOut>
+          <NavbarItem>
+            <NextLink href="/sign-up">
+              <Button>Sign Up</Button>
+            </NextLink>
+          </NavbarItem>
+        </SignedOut>
+        <SignedIn>
+          <NavbarItem>
+            <NextLink href={isAdmin ? '/dashboard' : '/student-dashboard'}>
+              <Button>Dashboard</Button>
+            </NextLink>
+          </NavbarItem>
+        </SignedIn>
         <NavbarMenuToggle />
       </NavbarContent>
 
@@ -75,10 +111,10 @@ export const Navbar = () => {
               <Link
                 color={
                   index === 2
-                    ? "primary"
+                    ? 'primary'
                     : index === siteConfig.navMenuItems.length - 1
-                    ? "danger"
-                    : "foreground"
+                    ? 'danger'
+                    : 'foreground'
                 }
                 href="/booking"
                 size="lg"
